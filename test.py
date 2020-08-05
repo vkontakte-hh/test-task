b8b23d3db4f1deb3c9c861a0639ca2120f0ca4cb
vkontakte-hh <69259731+vkontakte-hh@users.noreply.github.com>
2020-08-05 21:21:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello")
+import requests
+
+date = "2020-07-30"
+access_key = "c99033bf278986db036c4344d9d40f4a"
+symbols = ["USD", "AUD", "CAD", "PLN", "MXN"]
+URL = f"http://data.fixer.io/api/"
+data = requests.get(URL + date, params={"symbols": ','.join(symbols), "access_key": access_key})
+data = data.json()
+print(data)
