1dc1233a54a46308ea5655efb2f9569137ca3d1b
vkontakte-hh <69259731+vkontakte-hh@users.noreply.github.com>
2020-08-06 15:02:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,14 @@ def request():
     url = 'https://{host}:8443/?database={db}&query={query}'.format(
         host='rc1b-2kg8g5lblno2pln0.mdb.yandexcloud.net',
         db='vkontakte',
-        query='SHOW TABLES')
+        query="CREATE TABLE supply(
+                            supply_id INT PRIMARY KEY AUTO_INCREMENT, 
+                            title VARCHAR(50), 
+                            author VARCHAR(30), 
+                            price DECIMAL(8, 2), 
+                            amount INT
+                            )"
+    )
     auth = {
         'X-ClickHouse-User': 'user-vk',
         'X-ClickHouse-Key': 'Qqwerty123',
