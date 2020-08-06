d69f45fb13bc952e493c881de7b553e0709194bb
vkontakte-hh <69259731+vkontakte-hh@users.noreply.github.com>
2020-08-06 23:01:10 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,16 @@ def request():
     url = 'https://{host}:8443/?database={db}&query={query}'.format(
         host='rc1b-2kg8g5lblno2pln0.mdb.yandexcloud.net',
         db='vkontakte',
-        query='SELECT * FROM vkontakte.advertisements_data WHERE hour IN (17, 16, 15)'
+        query="""
+        CREATE TABLE vkontakte.advertisements_performance_data(hour UInt32,
+                                                               advertisementId UInt64,
+                                                               locationId UInt64,
+                                                               performanceRatio UInt8,
+                                                               samplingRate UInt8) 
+        ENGINE = MergeTree() PARTITION BY hour
+        ORDER BY (advertisementId, locationId, hour)
+        SETTINGS index_granularity = 8192;
+        """
     )
     auth = {
         'X-ClickHouse-User': 'user-vk',
