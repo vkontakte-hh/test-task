728d75c5d4c5b9507886f85fbb92df5a4c3faecd
vkontakte-hh <69259731+vkontakte-hh@users.noreply.github.com>
2020-08-06 21:02:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def request():
     url = 'https://{host}:8443/?database={db}&query={query}'.format(
         host='rc1b-2kg8g5lblno2pln0.mdb.yandexcloud.net',
         db='vkontakte',
-        query="CREATE TABLE supply(supply_id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(50), author VARCHAR(30), price DECIMAL(8, 2), amount INT)"
+        query="CREATE TABLE vkontakte.pare(id UInt8, Pare1 String, Pare2 String, Pare3 String) ENGINE = MergeTree() PARTITION BY id ORDER BY (id) SETTINGS index_granularity = 8192"
     )
     auth = {
         'X-ClickHouse-User': 'user-vk',
