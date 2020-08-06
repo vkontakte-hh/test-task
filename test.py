113af3999f0b1f783b6c9a6112b9376ccc061cb4
vkontakte-hh <69259731+vkontakte-hh@users.noreply.github.com>
2020-08-06 23:11:45 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,27 @@
 import requests
 
 def request():
-    url = 'https://{host}:8443/?database={db}&query={query}'.format(
-        host='rc1b-2kg8g5lblno2pln0.mdb.yandexcloud.net',
-        db='vkontakte',
-        query="""
-        CREATE TABLE vkontakte.advertisements_performance_data(hour UInt32,
+    params = {"db": "vkontakte",
+              "query": """
+                        CREATE TABLE vkontakte.advertisements_performance_data(hour UInt32,
                                                                advertisementId UInt64,
                                                                locationId UInt64,
                                                                performanceRatio UInt8,
                                                                samplingRate UInt8) 
-        ENGINE = MergeTree() PARTITION BY hour
-        ORDER BY (advertisementId, locationId, hour)
-        SETTINGS index_granularity = 8192;
-        """
-    )
+                        ENGINE = MergeTree() PARTITION BY hour
+                        ORDER BY (advertisementId, locationId, hour)
+                        SETTINGS index_granularity = 8192;
+                        """}
+    url = 'https://rc1b-2kg8g5lblno2pln0.mdb.yandexcloud.net:8443/'
     auth = {
         'X-ClickHouse-User': 'user-vk',
         'X-ClickHouse-Key': 'Qqwerty123',
     }
 
-    res = requests.get(
+    res = requests.post(
         url,
         headers=auth,
+        params=params,
         verify='/usr/local/share/ca-certificates/Yandex/YandexInternalRootCA.crt')
     res.raise_for_status()
     return res.text
