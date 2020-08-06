068bb0d979a4ae789803a197c39cc54832d9efcc
vkontakte-hh <69259731+vkontakte-hh@users.noreply.github.com>
2020-08-06 21:20:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def request():
     url = 'https://{host}:8443/?database={db}&query={query}'.format(
         host='rc1b-2kg8g5lblno2pln0.mdb.yandexcloud.net',
         db='vkontakte',
-        query=""" CREATE TABLE t_pare_table
+        query=""" 'CREATE TABLE t_pare_table
                                 (
                                     pare_id UInt8,
                                     Pare_one String,
@@ -14,7 +14,7 @@ def request():
                   ENGINE = MergeTree()
                   PARTITION BY pare_id 
                   ORDER BY (pare_id)
-                  SETTINGS index_granularity = 8192 
+                  SETTINGS index_granularity = 8192' 
               """
     )
     auth = {
