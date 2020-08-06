9dcbddf530650401a2228f94340270bc7c66630d
vkontakte-hh <69259731+vkontakte-hh@users.noreply.github.com>
2020-08-06 14:25:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def request():
     url = 'https://{host}:8443/?database={db}&query={query}'.format(
         host='rc1b-2kg8g5lblno2pln0.mdb.yandexcloud.net',
         db='vkontakte',
-        query='SELECT now()')
+        query='SELECT 1')
     auth = {
         'X-ClickHouse-User': 'user-vk',
         'X-ClickHouse-Key': 'Qqwerty123',
