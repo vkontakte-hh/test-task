5344c36a1250defdfb7ef43fa50b0364ea492d77
vkontakte-hh <69259731+vkontakte-hh@users.noreply.github.com>
2020-08-08 08:48:12 +0300

    Update test.py

--- test.py
+++ test.py
@@ -74,7 +74,7 @@ class ch_task:
                                       ORDER BY partition;
         """
         skip_days = self.select_query(select_skip_days_query)
-        return list(skip_days.text.strip())
+        return list(skip_days.text.split("\n"))
     
 ch = ch_task("user-vk", "Qqwerty123", "rc1b-2kg8g5lblno2pln0", "vkontakte")
 ch.check_or_create_tables()
